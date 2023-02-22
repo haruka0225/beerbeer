b5bf181ac1df8c21fbb7bbf51a026b1ab20aacc6
DESKTOP-J3PPFPG\harux <haru.xoxo7@gmail.com>
2023-02-22 22:59:37 +0900

    0222スタート

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function (){
+
+     console.log("HELLO");
+
+    // Jquery
+    // 送信ボタン押されたとき入力された値を出す
+
+    // console.log($(".post").val());
+
+    // $(".btn").on("click", function () {
+    //     console.log($(".post").val());
+
+
+    //     let postNum = $(".post").val();
+    //     // dataは必ず連想配列で書く
+
+    //     let data = {
+    //         zipcode: postNum,
+    //     }
+
+
+    //     // 連想配列文字変換したやつ　↓
+    //     $.ajax({
+    //         url: 'https://zipcloud.ibsnet.co.jp/api/search', //アクセスするURL
+    //         type: 'get', //post or get
+    //         cache: false,        //cacheを使うか使わないかを設定
+    //         dataType: 'json',     //data type script・xmlDocument・jsonなど
+    //         data: data,           //アクセスするときに必要なデータを記載      
+    //     })
+});
+
